c5a05d17e1c11bb1ff5e02b97f5987ae243740c4
Piotr Filipiak <kiddhkane@gmail.com>
2022-10-05 23:07:00 +0200

    Added custom values.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkCyan: "hsl(158, 36%, 37%)",
+        cream: "hsl(30, 38%, 92%)",
+        veryDarkBlue: "hsl(212, 21%, 14%)",
+        darkGrayishBlue: "hsl(228, 12%, 48%)",
+      },
+    },
+    fontFamily: {
+      montserrat: ["Montserrat", "sans-serif"],
+      fraunces: ["Fraunces", "sans-serif"],
+    },
   },
   plugins: [],
 };
